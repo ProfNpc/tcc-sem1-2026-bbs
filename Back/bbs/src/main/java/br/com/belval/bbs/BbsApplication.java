53030a3004ef58c08cd09de5a48545e3cea5c7cb
Felipe <rm101706@estudante.fieb.edu.br>
2026-03-06 21:51:14 -0300

    inicio

---
+++ Back/bbs/src/main/java/br/com/belval/bbs/BbsApplication.java
@@ -0,0 +1,13 @@
+package br.com.belval.bbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbsApplication.class, args);
+	}
+
+}
